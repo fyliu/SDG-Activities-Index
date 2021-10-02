27de23567825d10a74688180e03a46162035367d
Fang Liu <fangyiliu@gmail.com>
2021-10-02 11:34:29 -0700

    Update google-spreadsheet to 3.1.15
    The google sheet headers need to be changed back to the old wordings for
    this to work.

--- src/data.js
+++ src/data.js
@@ -1,49 +1,40 @@
-var GoogleSpreadsheet = require("google-spreadsheet");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
 var cred = require("./client-secret.json");
-const { promisify } = require("util");
 
 async function getSpreadsheet() {
-  const doc = new GoogleSpreadsheet(
-    `INSERT YOUR URL SHEET CODE HERE`,
-    null,
-    { gzip: false }
-  );
-  await promisify(doc.useServiceAccountAuth)(cred).catch((err) =>
-    console.log("Google Sheets Authentication Error")
-  );
-  const info = await promisify(doc.getInfo)();
-  const sheet = info.worksheets[0];
-  const rows = await promisify(sheet.getRows)();
-  return rows;
+  const doc = new GoogleSpreadsheet(URL_SHEET_CODE, null, {
+    gzip: false,
+  });
+  await doc
+    .useServiceAccountAuth(cred)
+    .catch((err) => console.log("Google Sheets Authentication Error"));
+  await doc.loadInfo();
+  const sheet = doc.sheetsByIndex[0];
+  const rows = await sheet.getRows();
+  return rows.filter((row) => row.activitytype);
 }
 
 async function getGoals() {
-  const sdgDoc = new GoogleSpreadsheet(
-    `INSERT YOUR URL SHEET CODE HERE`,
-    null,
-    { gzip: false }
-  );
-  await promisify(sdgDoc.useServiceAccountAuth)(cred).catch((err) =>
-    console.log("Google Sheets Authentication Error")
-  );
-  const sdgInfo = await promisify(sdgDoc.getInfo)();
-  const sdgSheet = sdgInfo.worksheets[2];
-  const sdgRows = await promisify(sdgSheet.getRows)();
+  const sdgDoc = new GoogleSpreadsheet(URL_SHEET_CODE, null, { gzip: false });
+  await sdgDoc
+    .useServiceAccountAuth(cred)
+    .catch((err) => console.log("Google Sheets Authentication Error"));
+  await sdgDoc.loadInfo();
+  const sdgSheet = sdgDoc.sheetsByIndex[2];
+  const sdgRows = await sdgSheet.getRows();
   return sdgRows;
 }
 
 async function addProject(data) {
-  const doc = new GoogleSpreadsheet(
-    `INSERT YOUR URL SHEET CODE HERE`,
-    null,
-    { gzip: false }
-  );
-  await promisify(doc.useServiceAccountAuth)(cred).catch((err) =>
-    console.log("Google Sheets Authentication Error")
-  );
+  const doc = new GoogleSpreadsheet(URL_SHEET_CODE, null, {
+    gzip: false,
+  });
+  await doc
+    .useServiceAccountAuth(cred)
+    .catch((err) => console.log("Google Sheets Authentication Error"));
 
-  const info = await promisify(doc.getInfo)();
-  const sheet = info.worksheets[1];
+  const info = await doc.loadInfo();
+  const sheet = doc.sheetsByIndex[1];
 
   sheet.addRow(data, function (err) {
     if (err) {
